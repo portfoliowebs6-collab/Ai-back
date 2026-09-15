0344c59955db76a99d906e1d52f100e71d4da734
portfoliowebs6-collab <portfoliowebs6@gmail.com>
2026-09-15 12:00:37 +0530

    Add files via upload

---
+++ backend/middleware/upload.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 15 * 1024 * 1024 }, // 15MB limit
+});
+
+module.exports = upload;
