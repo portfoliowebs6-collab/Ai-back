0e0b91acb5a8e5efa301e36b877c7f4eb8bbf7e0
portfoliowebs6-collab <portfoliowebs6@gmail.com>
2026-09-16 14:19:17 +0530

    Add files via upload

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+// Password save hone se pehle bcrypt se hash karna
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Password match karne ka method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
